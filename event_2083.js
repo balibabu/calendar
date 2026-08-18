7641abe946ed8ee9df0a79804d2feb516a1c3c62
balibabu <balibabuchauhan1@gmail.com>
2026-08-18 16:05:01 +0545

    added miti and dummy event

---
+++ event_2083.js
@@ -0,0 +1,12 @@
+window.BSEvents = Object.assign(window.BSEvents || {}, {
+  "2083-01-01": [
+    { title: "Nawa Barsha (New Year)", isPublicHoliday: true }
+  ],
+  "2083-01-11": [
+    { title: "Loktantra Diwas", isPublicHoliday: true }
+  ],
+  "2083-05-03": [
+    { title: "Buddha Jayanti", isPublicHoliday: true },
+    { title: "Ubhauli Parva", isPublicHoliday: false }
+  ]
+});
