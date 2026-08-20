d1fe2fbf6d17766ab80e313a7eed325aed0f0d8a
balibabu <balibabuchauhan1@gmail.com>
2026-08-20 18:35:49 +0545

    made production ready

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  content: ['./*.html', './js/**/*.js'],
+  theme: {
+    extend: {
+      colors: {
+        iosgray: {
+          400: '#8e8e93',
+          600: '#525252',
+          800: '#1c1c1e',
+          900: '#121214'
+        },
+        iosred: {
+          500: '#ff3b30',
+          600: '#e5332c'
+        }
+      }
+    }
+  }
+};
